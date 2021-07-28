d21d75e1193473e71edd394e641f90fe69dc8075
Megan McBride <megan.mcbride@colorado.edu>
2021-07-27 21:23:10 -0600

    Add sample data to the sample test files

---
+++ src/data/recipe-test-data.js
@@ -0,0 +1,100 @@
+const sampleRecipeData = [
+  {
+    "name": "all purpose flour",
+    "id": 20081,
+    "quantity": {
+      "amount": 1,
+      "unit": "cup"
+    }
+  },
+  {
+    "name": "baking powder",
+    "id": 18371,
+    "quantity": {
+      "amount": 2,
+      "unit": "teaspoons"
+    }
+  },
+  {
+    "name": "bananas",
+    "id": 9040,
+    "quantity": {
+      "amount": 12,
+      "unit": "servings"
+    }
+  },
+  {
+    "name": "buckwheat flour",
+    "id": 20011,
+    "quantity": {
+      "amount": 1,
+      "unit": "cup"
+    }
+  },
+  {
+    "name": "butter",
+    "id": 1001,
+    "quantity": {
+      "amount": 2,
+      "unit": "tablespoons"
+    }
+  },
+  {
+    "name": "butter",
+    "id": 1001,
+    "quantity": {
+      "amount": 6,
+      "unit": "tablespoons"
+    }
+  },
+  {
+    "name": "buttermilk",
+    "id": 1230,
+    "quantity": {
+      "amount": 2,
+      "unit": "cups"
+    }
+  },
+  {
+    "name": "eggs",
+    "id": 1123,
+    "quantity": {
+      "amount": 2,
+      "unit": ""
+    }
+  },
+  {
+    "name": "honey",
+    "id": 19296,
+    "quantity": {
+      "amount": 12,
+      "unit": "servings"
+    }
+  },
+  {
+    "name": "peanut butter",
+    "id": 16098,
+    "quantity": {
+      "amount": 12,
+      "unit": "servings"
+    }
+  },
+  {
+    "name": "salt",
+    "id": 2047,
+    "quantity": {
+      "amount": 1,
+      "unit": "teaspoon"
+    }
+  },
+  {
+    "name": "sugar",
+    "id": 19335,
+    "quantity": {
+      "amount": 2,
+      "unit": "teaspoons"
+    }
+  }
+]
+
+export default sampleRecipeData;

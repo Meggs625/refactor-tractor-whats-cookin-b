5edd73bad292075d23bd4f12600c874e3b614b16
Maria DelSignore <maria.ad27@gmail.com>
2021-07-28 16:09:52 -0600

    Test contructor properties

--- test/ingredient-test.js
+++ test/ingredient-test.js
@@ -5,9 +5,30 @@ import ingredientsData from '../src/data/ingredient-data.js';
 
 describe ('Ingredient', () => {
   let ingredient;
+  
   beforeEach(() => {
-    ingredient = new Ingredient();
+    ingredient = new Ingredient(ingredientsData[0]);
   });
   
+  it('should be a function', () => {
+    expect(Ingredient).to.be.a('function');
+  });
+
+  it('should be an instance of Ingredient', () => {
+    expect(ingredient).to.be.an.instanceOf(Ingredient);
+  });
+
+  it('should store an ingredient id', () => {
+    expect(ingredient.id).to.equal(20081);
+  });
+
+  it('should store an ingredient name', () => {
+    expect(ingredient.name).to.equal('wheat flour');
+  });
+
+  it('should store an ingredient cost', () => {
+    expect(ingredient.estimatedCostInCents).to.equal(142);
+  })
   
+
 });

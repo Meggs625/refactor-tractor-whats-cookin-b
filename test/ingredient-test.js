7e076679a1df4719837abd3995f48e04e2db9219
Maria DelSignore <maria.ad27@gmail.com>
2021-07-27 20:57:41 -0600

    Add initial describe and before each

--- test/ingredient-test.js
+++ test/ingredient-test.js
@@ -3,3 +3,11 @@ import { expect } from 'chai';
 import Ingredient from '../src/Ingredient.js';
 import ingredientsData from '../src/data/ingredient-data.js';
 
+describe ('Ingredient', () => {
+  let ingredient;
+  beforeEach(() => {
+    ingredient = new Ingredient();
+  });
+  
+  
+});

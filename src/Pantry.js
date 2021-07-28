ad7ae5a79294032414b1b275e9f815fe522f02a2
Megan McBride <megan.mcbride@colorado.edu>
2021-07-28 15:49:05 -0600

    Add notes to Pantry class

--- src/Pantry.js
+++ src/Pantry.js
@@ -4,7 +4,13 @@ class Pantry {
   }
 
   //Need to have the recipe ingredients array passed through as an argument? From the recipe class? 
+  //Method will compare the pantry.ingredient to the recipe.id and the pantry.amount to the recipe.quantity.amount
 
+  //could push quanity and ingredient into an array property of groceryList
+
+  //ingredients method to return name based on ID - could be used if passed id from groceryList array
+
+  //
 
 
 }

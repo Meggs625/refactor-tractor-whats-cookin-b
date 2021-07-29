ded28dcb5512d36279b3477412cb27032247ac83
Megan McBride <megan.mcbride@colorado.edu>
2021-07-28 19:31:30 -0600

    Add GET fetch function

---
+++ src/apiCalls.js
@@ -0,0 +1,6 @@
+const getData = (dataSet) => {
+  return fetch(`http://localhost:3001/api/v1/${dataSet}`)
+    .then(response => response.json)
+    .then(data => data)
+    .catch(err => console.log(`${dataSet} error: ${err}`))
+}

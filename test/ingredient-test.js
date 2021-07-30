514d2dc9e509e05584d07b9509ff6c25034b76d7
Maria DelSignore <maria.ad27@gmail.com>
2021-07-30 13:21:31 -0600

    Import repository file and update before each

--- test/ingredient-test.js
+++ test/ingredient-test.js
@@ -1,16 +1,16 @@
 import { expect } from 'chai';
-
 import Ingredient from '../src/Ingredient.js';
+import IngredientRepository from '../src/IngredientRepository.js';
 import ingredientsData from '../src/data/ingredient-data.js';
 
-describe ('Ingredient', () => {
-  let ingredient, ingredient2;
+describe('Ingredient', () => {
+  let ingredientRepo, ingredient, ingredient2;
   
   beforeEach(() => {
+    ingredientRepo = new Ingredient(ingredientsData);
     ingredient = new Ingredient(20081, 'wheat flour', 142);
     ingredient2 = new Ingredient(12061, 'whole almonds', 1029)
-
-  })
+  });
   
   it('should be a function', () => {
     expect(Ingredient).to.be.a('function');
@@ -29,12 +29,12 @@ describe ('Ingredient', () => {
   });
 
   it('should store an ingredient cost', () => {
-    expect(ingredient.estimatedCostInCents).to.equal(142);
+    expect(ingredient.cost).to.equal(142);
   });
   
   it('should have a way to return the ingredient name', () => {
-    expect(ingredient.returnIngredientName()).to.equal('wheat flour');
-    expect(ingredient2.returnIngredientName()).to.equal('whole almonds');
+    expect(ingredient.returnIngredientName(20081)).to.equal('wheat flour');
+    expect(ingredient2.returnIngredientName(12061)).to.equal('whole almonds');
   });
 
   it('should have a way to return the cost in dollars', () => {

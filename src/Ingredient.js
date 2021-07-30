6ec0c97af14f7034db460ca4622d5539e6971cda
Maria DelSignore <maria.ad27@gmail.com>
2021-07-30 13:23:03 -0600

    Update properties based on new file

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,22 +1,23 @@
 class Ingredient {
-  constructor(id, itemName, cost) {
+  constructor(id, name, cost, data) {
     this.id = id;
-    this.name = itemName;
-    this.estimatedCostInCents = cost;
+    this.name = name;
+    this.cost = cost;
+    this.ingredientsRepo = data;
   }
 
-  returnIngredientName() {
+  returnIngredientName(id) {
     return this.name;
   }
 
   updateCostToDollars(id, cost) {
-    this.estimatedCostInCents = Math.floor(cost) / 100;
-    return this.estimatedCostInCents;
+    this.cost = Math.floor(cost) / 100;
+    return this.cost;
   }
 
   calculateTotalIngredientCost(id, quantity) {
-    let total = this.estimatedCostInCents * quantity;
-    return this.updateCostToDollars(id, total)
+    let total = this.cost * quantity;
+    return this.updateCostToDollars(id, total);
   }
 }
 
---
+++ src/IngredientRepository.js
@@ -0,0 +1,7 @@
+class IngredientRepository {
+  constructor(data) {
+    this.data = data;
+  }
+}
+
+export default IngredientRepository;

cb041d7aca90bda139d4a275a13f5d944dbbc6b9
Maria DelSignore <maria.ad27@gmail.com>
2021-08-02 15:55:07 -0600

    Remove unneeded code from dom updates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,14 +1,8 @@
-
-
-
 const domUpdates = {
 
-  // maxlength="40"
-
-// CREATE RECIPE CARD
-renderRecipeCard(recipeInfo, shortRecipeName) {
-  let main = document.querySelector("main");
-  let cardHtml = `
+  renderRecipeCard(recipeInfo, shortRecipeName) {
+    let main = document.querySelector("main");
+    let cardHtml = `
 
     <div class="recipe-card" id=${recipeInfo.id}>
       <h3 class="recipe-name">${shortRecipeName}</h3>
@@ -24,7 +18,6 @@ renderRecipeCard(recipeInfo, shortRecipeName) {
     main.insertAdjacentHTML("beforeend", cardHtml);
   },
 
-  // LIST TAGS
   renderTags(allTags) {
     let tagList = document.querySelector(".tag-list");
     allTags.forEach(tag => {
@@ -37,8 +30,6 @@ renderRecipeCard(recipeInfo, shortRecipeName) {
     });
   },
   
-
-  // CREATE RECIPE INSTRUCTIONS
   renderRecipeInfo() {
     let fullRecipeInfo = document.querySelector(".recipe-instructions")
 
@@ -57,21 +48,12 @@ renderRecipeCard(recipeInfo, shortRecipeName) {
     fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
   },
 
-  renderRecipeIngredients(ingredients) {
-    // let fullRecipeInfo = document.querySelector(".recipe-instructions")
-    // fullRecipeInfo.insertAdjacentElement('', )
-
-    // `${capitalize(i.name)} (${i.quantity.amount} ${i.quantity.unit})`
-  },
-
   renderRecipeInstructions(bloop) {
     let fullRecipeInfo = document.querySelector(".recipe-instructions")
 
     fullRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
     fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
   },
-
-
 
   renderRecipeInstructions(instructionsList) {
     let fullRecipeInfo = document.querySelector(".recipe-instructions")
@@ -80,33 +62,10 @@ renderRecipeCard(recipeInfo, shortRecipeName) {
     fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
   },
 
-
   renderRecipeImage(recipe) {
     let fullRecipeInfo = document.querySelector(".recipe-instructions")
-    // let recipeTitle = document.getElementById("recipe-title")
-    // .style.backgroundImage = `url(${recipe})`;
     fullRecipeInfo.insertAdjacentHTML('afterbegin', `<img class="recipe-title" src="${recipe}" alt="recipe image" />`)
   },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   renderAllRecipes(recipes) {
     recipes.forEach(recipe => {
@@ -117,7 +76,6 @@ renderRecipeCard(recipeInfo, shortRecipeName) {
 
   renderPantryInfo(pantry) {
     pantry.forEach(ingredient => {
-      //updated id="${ingredient.name} to ${ingredient.id}"
       let ingredientHtml = 
       `<li>
       <input type="checkbox" name="${ingredient.name}"class="pantry-checkbox" id="${ingredient.id}">
@@ -132,14 +90,7 @@ renderRecipeCard(recipeInfo, shortRecipeName) {
     let domRecipe = document.getElementById(`${recipe.id}`);
     domRecipe.style.display = "none";
   }
-
-
-
-
-
-
-
-
 }
+
 
 export default domUpdates;
